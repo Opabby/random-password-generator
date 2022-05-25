f7e66ac6ee59ccf59c06a15027675a3014143771
Thaís Rodeiro <ttheinterpreter@gmail.com>
2022-05-25 18:52:29 -0300

    adding functions

---
+++ main.js
@@ -0,0 +1,37 @@
+let passwordElements = document.querySelectorAll(".password-box");
+let asciiList = [];
+
+function fetchPasswords() {
+    buildAsciiList();
+
+    for(let i = 0; i < passwordElements.length; i++) {
+        passwordElements[i].classList.remove("has-dots");
+        passwordElements[i].classList.add("has-password");
+        passwordElements[i].value = generatePassword(10);
+    }
+    asciiList = [];
+}
+
+function generatePassword(size) {
+    let numberList = getRandomNumberList(size);
+    let password = "";
+
+    for (let i = 0; i < size; i++) {
+        password += asciiList[numberList[i]];
+    }
+    return password;
+}
+
+function buildAsciiList() {
+    for (let i = 33; i < 127; i++) {
+        asciiList.push(String.fromCharCode(i));
+    }
+}
+
+function getRandomNumberList(size) {
+    let list = [];
+    for (let i = 0; i < size; i++) {
+        list.push(Math.floor(Math.random() * asciiList.length));
+    }
+    return list;
+}
